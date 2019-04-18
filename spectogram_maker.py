7e4c0fb49ea0df59101a0d4eeedae2f55b480648
Noah Haselow <nhaselow@gmail.com>
2019-04-17 21:22:55 -0500

    Added spectogram_maker to convert .wav.gz to .png files

---
+++ spectogram_maker.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+#coding: utf-8
+""" This work is licensed under a Creative Commons Attribution 3.0 Unported License.
+    Frank Zalkow, 2012-2013 """
+
+import os
+import gzip
+import shutil
+import numpy as np
+from matplotlib import pyplot as plt
+import scipy.io.wavfile as wav
+from numpy.lib import stride_tricks
+
+""" short time fourier transform of audio signal """
+def stft(sig, frameSize, overlapFac=0.5, window=np.hanning):
+    win = window(frameSize)
+    hopSize = int(frameSize - np.floor(overlapFac * frameSize))
+
+    # zeros at beginning (thus center of 1st window should be for sample nr. 0)   
+    samples = np.append(np.zeros(int(np.floor(frameSize/2.0))), sig)    
+    # cols for windowing
+    cols = np.ceil( (len(samples) - frameSize) / float(hopSize)) + 1
+    # zeros at end (thus samples can be fully covered by frames)
+    samples = np.append(samples, np.zeros(frameSize))
+
+    frames = stride_tricks.as_strided(samples, shape=(int(cols), frameSize), strides=(samples.strides[0]*hopSize, samples.strides[0])).copy()
+    frames *= win
+
+    return np.fft.rfft(frames)    
+
+""" scale frequency axis logarithmically """    
+def logscale_spec(spec, sr=44100, factor=20.):
+    timebins, freqbins = np.shape(spec)
+
+    scale = np.linspace(0, 1, freqbins) ** factor
+    scale *= (freqbins-1)/max(scale)
+    scale = np.unique(np.round(scale))
+
+    # create spectrogram with new freq bins
+    newspec = np.complex128(np.zeros([timebins, len(scale)]))
+    for i in range(0, len(scale)):        
+        if i == len(scale)-1:
+            newspec[:,i] = np.sum(spec[:,int(scale[i]):], axis=1)
+        else:        
+            newspec[:,i] = np.sum(spec[:,int(scale[i]):int(scale[i+1])], axis=1)
+
+    # list center freq of bins
+    allfreqs = np.abs(np.fft.fftfreq(freqbins*2, 1./sr)[:freqbins+1])
+    freqs = []
+    for i in range(0, len(scale)):
+        if i == len(scale)-1:
+            freqs += [np.mean(allfreqs[int(scale[i]):])]
+        else:
+            freqs += [np.mean(allfreqs[int(scale[i]):int(scale[i+1])])]
+
+    return newspec, freqs
+
+""" plot spectrogram"""
+def plotstft(audiopath, binsize=2**10, plotpath=None, colormap="jet"):
+    samplerate, samples = wav.read(audiopath)
+
+    s = stft(samples, binsize)
+
+    sshow, freq = logscale_spec(s, factor=1.0, sr=samplerate)
+
+    ims = 20.*np.log10(np.abs(sshow)/10e-6) # amplitude to decibel
+
+    timebins, freqbins = np.shape(ims)
+
+    #print("timebins: ", timebins)
+    #print("freqbins: ", freqbins)
+
+    plt.figure(figsize=(15, 7.5))
+    plt.imshow(np.transpose(ims), origin="lower", aspect="auto", cmap=colormap, interpolation="none")
+
+    plt.colorbar()
+
+    plt.xlabel("time (s)")
+    plt.ylabel("frequency (hz)")
+    plt.xlim([0, timebins-1])
+    plt.ylim([0, freqbins])
+
+    # remove whitespace and axes
+    #plt.gca().set_axis_off()
+    plt.subplots_adjust(top = 1, bottom = 0, right = 1, left = 0, hspace = 0, wspace = 0)
+    plt.margins(0,0)
+    plt.gca().xaxis.set_major_locator(plt.NullLocator())
+    plt.gca().yaxis.set_major_locator(plt.NullLocator())
+
+    if plotpath:
+        plt.savefig(plotpath, bbox_inches="tight")
+    else:
+        plt.show()
+
+    plt.clf()
+
+    return ims
+
+aud_dir_path = './audio_files/'
+unzipped_dir_path = './unzipped_audio_files/'
+img_dir_path = './spectograms/'
+size = len(os.listdir(aud_dir_path))
+i = 1
+for audfile in os.listdir(aud_dir_path) :
+	if(audfile.endswith('.wav.gz')):
+		#unzip
+		aud_file_path = aud_dir_path+audfile
+		unzipped_file_path = unzipped_dir_path+audfile[:-3]
+		img_file_path = img_dir_path+audfile[:-7]+'.png'
+		with gzip.open(aud_file_path, 'rb') as f_in:
+			with open(unzipped_file_path, 'wb') as f_out:
+				shutil.copyfileobj(f_in, f_out)
+		#convert to spectogram
+		ims = plotstft(unzipped_file_path, plotpath=img_file_path)
+		print(str(i) + ' of ' + str(size) + ' converted.')
+		i = i+1
+
+print('done!')
+
+
+
